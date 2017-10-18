022c5f7bc8ae9b58b129f01e38a03e556e2d48e1
Diego Rodríguez <diegorbaquero@gmail.com>
2017-10-18 17:33:45 -0500

    Headless

--- Ghost/It2/gremlins/wdio-chrome.conf.js
+++ Ghost/It2/gremlins/wdio-chrome.conf.js
@@ -1,25 +1,28 @@
 exports.config = {
-    specs: [
-        './tests/*.js'
-    ],
-    maxInstances: 10,
-    capabilities: [{
-        maxInstances: 5,
-        browserName: 'chrome'
-    }],
-    sync: true,
-    logLevel: 'verbose',
-    coloredLogs: true,
-    bail: 0,
-    screenshotPath: './shots/',
-    baseUrl: 'http://localhost:2368',
-    waitforTimeout: 30000,
-    connectionRetryTimeout: 60000,
-    connectionRetryCount: 1,
-    services: ['selenium-standalone'],
-    framework: 'jasmine',
-    reporters: ['spec'],
-    jasmineNodeOpts: {
-        defaultTimeoutInterval: 60000,
+  specs: [
+    './tests/*.js'
+  ],
+  maxInstances: 10,
+  capabilities: [{
+    maxInstances: 5,
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=1280,800'],
     }
+  }],
+  sync: true,
+  logLevel: 'verbose',
+  coloredLogs: true,
+  bail: 0,
+  screenshotPath: './shots/',
+  baseUrl: 'http://localhost:2368',
+  waitforTimeout: 30000,
+  connectionRetryTimeout: 60000,
+  connectionRetryCount: 1,
+  services: ['selenium-standalone'],
+  framework: 'jasmine',
+  reporters: ['spec'],
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 60000,
+  }
 }
